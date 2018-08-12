337530671273c5928f1b1e990eba4b7810f18c97
Mike Rivera <rivera.mj@gmail.com>
2018-08-12 19:25:40 +0200

    add create order

--- src/main/scala/com/mypetdefense/shipstation/Order.scala
+++ src/main/scala/com/mypetdefense/shipstation/Order.scala
@@ -8,6 +8,8 @@ import net.liftweb.util.Helpers._
 
 import dispatch._, Defaults._
 
+import scala.collection.JavaConversions._
+
 case class Order(
   orderId: Int,
   orderNumber: String,
@@ -54,6 +56,78 @@ case class Order(
 }
 
 object Order extends Gettable[Order] {
-  def baseResourceCalculator(req: Req) =
-    req / "orders"
- }
+  def baseResourceCalculator(req: Req) = req / "orders"
+  
+   def create(
+    orderNumber: String,
+    orderKey: Option[String] = None,
+    orderDate: String,
+    paymentDate: Option[String] = None,
+    shipByDate: Option[String] = None,
+    orderStatus: String,
+    customerUsername: Option[String] = None,
+    customerEmail: Option[String] = None,
+    billTo: Address,
+    shipTo: Address,
+    items: Option[List[OrderItem]] = None,
+    taxAmount: Option[Double] = None,
+    shippingAmount: Option[Double] = None,
+    customerNotes: Option[String] = None,
+    internalNotes: Option[String] = None,
+    gift: Boolean = false,
+    giftMessage: Option[String] = None,
+    paymentMethod: Option[String] = None,
+    requestedShippingService: Option[String] = None,
+    carrierCode: Option[String] = None,
+    serviceCode: Option[String] = None,
+    packageCode: Option[String] = None,
+    confirmation: Option[String] = None,
+    shipDate: Option[String] = None,
+    weight: Option[Weight] = None,
+    dimensions: Option[Dimensions] = None,
+    insuranceOptions: Option[InsuranceOptions] = None,
+    advancedOptions: Option[AdvancedOptions] = None,
+    tagIds: Option[List[Int]] = None
+  )(implicit exec: ShipStationExecutor): Future[Box[Order]] = {
+    val requiredParams = Map(
+      "orderNumber" -> orderNumber,
+      "orderDate" -> orderDate,
+      "orderStatus" -> orderStatus,
+      "shipTo" -> compact(render(Extraction.decompose(shipTo))),
+      "billTo" -> compact(render(Extraction.decompose(billTo)))
+    )
+
+    val optionalParams = List(
+      orderKey.map(("orderKey", _)),
+      paymentDate.map(("paymentDate", _)),
+      shipByDate.map(("shipByDate", _)),
+      customerUsername.map(("customerUsername", _)),
+      customerEmail.map(("customerEmail", _)),
+      items.map(i => ("items", compact(render(Extraction.decompose(i))))),
+      taxAmount.map(t => ("taxAmount", t.toString)),
+      shippingAmount.map(s => ("shippingAmount", s.toString)),
+      customerNotes.map(("customerNotes", _)),
+      internalNotes.map(("internalNotes", _)),
+      giftMessage.map(("giftMessage", _)),
+      paymentMethod.map(("paymentMethod", _)),
+      requestedShippingService.map(("requestedShippingService", _)),
+      carrierCode.map(("carrierCode", _)),
+      serviceCode.map(("serviceCode", _)),
+      packageCode.map(("packageCode", _)),
+      confirmation.map(("confirmation", _)),
+      shipDate.map(("shipDate", _)),
+      weight.map(w => ("weight", compact(render(Extraction.decompose(w))))),
+      dimensions.map(d => ("dimensions", compact(render(Extraction.decompose(d))))),
+      insuranceOptions.map(i => ("insuranceOptions", compact(render(Extraction.decompose(i))))),
+      advancedOptions.map(a => ("advancedOptions", compact(render(Extraction.decompose(a))))),
+      weight.map(w => ("weight", compact(render(Extraction.decompose(w))))),
+      tagIds.map(t => ("tagIds", compact(render(Extraction.decompose(t)))))
+    ).flatten.toMap
+
+    val params = ShipStationHelpers.fixMultiLevelDecomposing(compact(render(requiredParams ++ optionalParams)))
+
+    val uri = baseResourceCalculator(exec.baseReq <:< Map("Content-Type" -> "application/json")) / "createorder" << params
+
+    exec.executeFor[Order](uri)
+  }
+}
