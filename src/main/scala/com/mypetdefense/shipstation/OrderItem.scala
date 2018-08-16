914e73a737e1b290bf5141280be7fa7e7e53e5b6
Mike Rivera <rivera.mj@gmail.com>
2018-08-16 14:40:43 +0200

    update OrderItem model

--- src/main/scala/com/mypetdefense/shipstation/OrderItem.scala
+++ src/main/scala/com/mypetdefense/shipstation/OrderItem.scala
@@ -10,24 +10,28 @@ import dispatch._, Defaults._
 
 case class OrderItem(
   orderItemId: Int,
-  lineItemKey: Option[String],
-  sku: Option[String],
-  name: Option[String],
-  imageUrl: Option[String],
-  weight: Option[Weight],
+  lineItemKey: Option[String] = None,
+  sku: Option[String] = None,
+  name: Option[String] = None,
+  imageUrl: Option[String] = None,
+  weight: Option[Weight] = None,
   quantity: Int,
-  unitPrice: Option[Double],
-  taxAmount: Option[Double],
-  shippingAmount: Option[Double],
-  warehouseLocation: Option[String],
-  options: List[ItemOption],
-  productId: Option[Int],
-  fulfillmentSku: Option[String],
-  adjustment: Boolean,
-  upc: Option[String],
-  createDate: String,
-  modifyDate: String,
+  unitPrice: Option[Double] = None,
+  taxAmount: Option[Double] = None,
+  shippingAmount: Option[Double] = None,
+  warehouseLocation: Option[String] = None,
+  options: Option[List[ItemOption]] = None,
+  productId: Option[Int] = None,
+  fulfillmentSku: Option[String] = None,
+  adjustment: Option[Boolean] = None,
+  upc: Option[String] = None,
+  createDate: Option[String] = None,
+  modifyDate: Option[String] = None,
   raw: Option[JValue] = None
 ) extends ShipStationObject {
   def withRaw(raw: JValue) = this.copy(raw = Some(raw))
+}
+
+object OrderItem extends Gettable[OrderItem] {
+  def baseResourceCalculator(req: Req) = req / "products"
 }
